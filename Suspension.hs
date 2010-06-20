9c1b91da7cd82337931afdca77c72870a70ed8ff
Jason McCarty <jmccarty@sent.com>
2010-06-19 22:54:52 -0400

    Implement Steenrod operations on a suspension.

--- Suspension.hs
+++ Suspension.hs
@@ -1,15 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Suspension(Suspend) where
 import Grading
 import Natural
 import FreeModule
 import qualified Data.Set as S
+import Steenrod
+import qualified Restricted as R
+import Basis
 
 newtype Basis n b = Basis { unBasis :: b } deriving (Eq, Ord)
 
 pack = Basis
 unpack = unBasis
+
+instance R.MonadR (Basis n) b where
+  return = pack
 
 instance (Nat n, Show b) => Show (Basis n b) where
   showsPrec p (b :: Basis n b) = showParen (p > 5) $ showString "\\sigma^{"
@@ -24,3 +32,6 @@ instance (Ord b, Graded b, Nat n) => Graded (Basis n b) where
     where n' = toNum (undefined :: n)
 
 type Suspend n m = FreeModule (URing m) (Basis n (UBasis m))
+
+instance (Ord b, AModule b) => AModule (Basis n b) where
+  sq' m = include . sq' m . unpack
